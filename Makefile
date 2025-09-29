9f09e0fed1c1ea8d518e310819a527542d22b905
TaDevTech Authority <nl5598835@gmail.com>
2025-09-29 17:25:22 +0700

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,36 +1,33 @@
 export THEOS=/var/theos
-ARCHS = arm64 #arm64e
+ARCHS = arm64
 DEBUG = 0
 FINALPACKAGE = 1
 FOR_RELEASE = 1
 IGNORE_WARNINGS=0
 MOBILE_THEOS=1
 
-
 include $(THEOS)/makefiles/common.mk
 
 TWEAK_NAME = ThienNhanDev
 
-
 PROJ_COMMON_FRAMEWORKS = UIKit Foundation Security QuartzCore CoreGraphics CoreText
 
-
-ThienNhanDev_CFLAGS = -fobjc-arc
+ThienNhanDev_CFLAGS = -fobjc-arc -I$(THEOS_PROJECT_DIR)/imgui
 ThienNhanDev_CCFLAGS = -std=c++11 -fno-rtti -fno-exceptions -DNDEBUG
 
-
-ThienNhanDev_FILES = main.mm $(wildcard SCLAlertView/*.m) 
+ThienNhanDev_FILES = \
+    main.mm \
+    FPSDisplay.m \
+    $(wildcard SCLAlertView/*.m) \
+    imgui/imgui.cpp \
+    imgui/imgui_draw.cpp \
+    imgui/imgui_widgets.cpp \
+    imgui/imgui_tables.cpp \
+    imgui/imgui_demo.cpp
 
 ThienNhanDev_LIBRARIES += substrate
-
 ThienNhanDev_FRAMEWORKS = $(PROJ_COMMON_FRAMEWORKS)
 
-ThienNhanDev_LDFLAGS += libAPIKey.a
-
+ThienNhanDev_LDFLAGS += API/libTKAPIKey.a
 
 include $(THEOS_MAKE_PATH)/tweak.mk
-
-
-	
-
-
--- main.mm
+++ main.mm
@@ -7,123 +7,119 @@ blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
 #import "main.h"
 #import "SCLAlertView/SCLAlertView.h"
-#import "APIKey.h"
+#import "API/APIClient.h"
 
-#import "imgui.h"
-#import "imconfig.h"
-#import "imgui_internal.h"
-#import "imstb_rectpack.h"
-#import "imstb_textedit.h"
-#import "imstb_truetype.h"
+#import "IMGUI/imgui.h"
+#import "IMGUI/imconfig.h"
+#import "IMGUI/imgui_internal.h"
+#import "IMGUI/imstb_rectpack.h"
+#import "IMGUI/imstb_textedit.h"
+#import "IMGUI/imstb_truetype.h"
 
 //#import "imgui_impl_metal.h"
 
-#import "zzz.h"
-
-SCLAlertView *alert;
-
+#import "IMGUI/zzz.h"
 
 @implementation NguyenNamFramework
 
-
-PPAPIKey *APIKey = [[PPAPIKey alloc] init];
-
-
-
+static SCLAlertView *alert;
 static NguyenNamFramework *active;
 
++ (void)load {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 
-+ (void)load
-{
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3* NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        alert = [[SCLAlertView alloc] initWithNewWindow];  
 
-alert = [[SCLAlertView alloc] initWithNewWindow];  
+        TKAPIKey *APIClient = [TKAPIKey sharedAPIClient];
 
-        PPAPIKey *APIKey = [[PPAPIKey alloc] init];
+        [APIClient setPackageToken:@"6koz9fnxflrcgnpbhj7lwk8wr6qvv3r1kist384oimuyw=fdbu3g94qdaqm8cmh7nj40u0te/eoy95tycidp21v2hxa6j0slzsbp_7e1zag525x"];
+        [APIClient setPackageVersion:@"1.0"]; 
+        [APIClient setENLanguage:NO]; 
 
-        [APIKey setPackageToken:NSSENCRYPT("KFcvy8VDro9dIbr4Guv8KK2HREylEmLwhvIh8BuEvYJ29PfurMGF1M87e5RVS3XDnF14tbGNUSgKDz8xdz3X9K5euH5XJp8ROAeT")];
-        [APIKey setAppVersion:NSSENCRYPT("1.0.0")];
-        [APIKey setENLanguage:YES];
+        [APIClient paid:^{
+            active = [NguyenNamFramework new];
+            [active start];
 
-        [APIKey loading:^{
-        active =  [NguyenNamFramework new];
-        [active start];
-
-}];
-});
-}
-
--(void)start{
-
-alert.shouldDismissOnTapOutside = YES;//Replace with NO if you want the user to force a button in the alert
-
-alert.customViewColor = UIColorFromHEX(0x474747);
-  
-
-alert.showAnimationType = SCLAlertViewShowAnimationSlideInToCenter;   
-
-alert.backgroundType = SCLAlertViewBackgroundBlur;
-
-alert.cornerRadius = 20.0f;
-
-alert.backgroundViewColor = UIColorFromHEX(0x302d26);
-
-
-ImGui::Text("Key: %s", [[APIK getKey] UTF8String]);
-ImGui::Text("Thời Hạn: %s", [[APIK getKeyExpire] UTF8String]);
-ImGui::Text("UUID: %s", [[APIK getUDID] UTF8String]);
-ImGui::Text("Tên Thiết Bị: %s", [[APIK getDeviceName] UTF8String]);
-ImGui::Text("Phiên Bản iOS: %s", [[APIK getiOSVersion] UTF8String]);
-ImGui::Text("Trạng Thái Jailbreak: %s", [[APIK getJailbreakStatus] UTF8String]);
-
-
-//Button
-
-//Demo Button
-/*
-[alert addButton:@"Name Button" actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"URL YOU WANT TO OPEN WHEN CLICK"] options:@{} completionHandler:nil];
-}];
-*/
-
-
-//Example button
-[alert addButton:@"Zalo Group " actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://zalo.me/g/pzmvdd558"] options:@{} completionHandler:nil];
-}];
-
-[alert addButton:@"TELEGRAM Dev ThienNhan " actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://t.me/devthiennhan"] options:@{} completionHandler:nil];
-}];
-
-[alert addButton:@"Tiktok Dev ThienNhan " actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://www.tiktok.com/@devthiennhan?_t=ZS-906TdX9vf67&_r=1"] options:@{} completionHandler:nil];
-    }];
-
-[alert addButton:@"Facebook Dev ThienNhan " actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://facebook.com/devthiennhan"] options:@{} completionHandler:nil];
-    }];
-
-[alert addButton:@"Website Dev ThienNhan " actionBlock:^(void) {
-[[UIApplication sharedApplication] openURL:[NSURL URLWithString: @"https://devthiennhan.bio.link/"] options:@{} completionHandler:nil];
+            // Optional logs
+            NSLog(@"APIData - Key: %@", [APIClient getKey]);
+            NSLog(@"APIData - UDID: %@", [APIClient getUDID]);
+            NSLog(@"APIData - Expiry date: %@", [APIClient getExpiryDate]);
+            NSLog(@"APIData - Device model: %@", [APIClient getDeviceModel]);
         }];
-//End example button
-
-
-
-
-//You can replace any logo link with a direct image link
-NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://sf-static.upanhlaylink.com/img/image_20250928e1675e244f7ec1d59e82d9dd5e74ebe8.jpg"]];
-
-UIImage* customAlertImage = [UIImage imageWithData:data];  
- 
-[alert showCustom:alert image:customAlertImage color:[UIColor clearColor] title:@"Dev ThienNhan " subTitle:@"Cảm ơn bạn đã sử dụng  iOS Crack Dev .\nBấm vào Logo để tắt thông báo." closeButtonTitle:nil duration:9999999999.0f];
-
-
-
+    });
 }
 
+- (void)start {
+    alert.shouldDismissOnTapOutside = YES;
+    alert.customViewColor = UIColorFromHEX(0x474747);
+    alert.showAnimationType = SCLAlertViewShowAnimationSlideInToCenter;   
+    alert.backgroundType = SCLAlertViewBackgroundBlur;
+    alert.cornerRadius = 20.0f;
+    alert.backgroundViewColor = UIColorFromHEX(0x302d26);
 
+    TKAPIKey *APIClient = [TKAPIKey sharedAPIClient];
+
+    const char *getKey = [[APIClient getKey] UTF8String];
+    const char *getExpiryDate = [[APIClient getExpiryDate] UTF8String];
+    const char *getUDID = [[APIClient getUDID] UTF8String];
+    const char *deviceName = [[APIClient deviceName] UTF8String];
+    const char *deviceModel = [[APIClient getDeviceModel] UTF8String];
+    const char *iosver = [[APIClient osVersion] UTF8String];
+    const char *ip = [[APIClient ipAddress] UTF8String];
+
+    ImGui::Text("Key: %s", getKey);
+    ImGui::Text("Hạn Sử Dụng: %s", getExpiryDate);
+    ImGui::Text("UDID: %s", getUDID);
+    ImGui::Text("DEVICE NAME: %s", deviceName);
+    ImGui::Text("DEVICE MODEL: %s", deviceModel);
+    ImGui::Text("VERSION: %s", iosver);
+    ImGui::Text("IP: %s", ip);
+
+    if (ImGui::Button("Reload")) {
+        [APIClient reloadInfo];
+    }
+
+    ImGui::SameLine(); 
+
+    if (ImGui::Button("Copy Key")) {
+        NSString *key = [APIClient getKey];
+        if (key != nil && key.length > 0) {
+            [APIClient copyKey:key]; 
+        }
+    }
+
+    // Example buttons
+    [alert addButton:@"Zalo Group " actionBlock:^(void) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://zalo.me/g/pzmvdd558"] options:@{} completionHandler:nil];
+    }];
+
+    [alert addButton:@"TELEGRAM Dev ThienNhan " actionBlock:^(void) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://t.me/devthiennhan"] options:@{} completionHandler:nil];
+    }];
+
+    [alert addButton:@"Tiktok Dev ThienNhan " actionBlock:^(void) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.tiktok.com/@devthiennhan?_t=ZS-906TdX9vf67&_r=1"] options:@{} completionHandler:nil];
+    }];
+
+    [alert addButton:@"Facebook Dev ThienNhan " actionBlock:^(void) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://facebook.com/devthiennhan"] options:@{} completionHandler:nil];
+    }];
+
+    [alert addButton:@"Website Dev ThienNhan " actionBlock:^(void) {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://devthiennhan.bio.link/"] options:@{} completionHandler:nil];
+    }];
+
+    // Logo image
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"https://sf-static.upanhlaylink.com/img/image_20250928e1675e244f7ec1d59e82d9dd5e74ebe8.jpg"]];
+    UIImage *customAlertImage = [UIImage imageWithData:data];  
+
+    [alert showCustom:alert
+                image:customAlertImage
+                color:[UIColor clearColor]
+                title:@"Dev ThienNhan "
+             subTitle:@"Cảm ơn bạn đã sử dụng  iOS Crack Dev .\nBấm vào Logo để tắt thông báo."
+     closeButtonTitle:nil
+             duration:9999999999.0f];
+}
 
 @end
-make package
